52e499228285a6bd7a980ad22c68578c9b662b31
A. Mohammed Jasil <jasiljazz@gmail.com>
2016-11-13 17:53:18 +0530

    added comments to the model

--- model/link.js
+++ model/link.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-
+//define the schema
 var linkSchema = new Schema({
   requestURL : {type: String , required:true},
   shortURL : {type: String, required:true}
 });
 
 linkSchema.methods.genURL = function(){
-  var genUrl="https://jasil1414-urlshortner.herokuapp.com/";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-  for(i=0; i<=6; i++){
+  var genUrl="https://jasil1414-urlshortner.herokuapp.com/";//heroku prefix link
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"//generate url based on these characters
+  for(i=0; i<=6; i++){//6 characters
     genUrl += possible.charAt(Math.floor(Math.random()*possible.length));
 
   }
